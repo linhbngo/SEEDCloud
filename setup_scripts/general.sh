76ca353b4ce18e3442403340e93507fe60c70b61
Linh B Ngo <lngo@clemson.edu>
2018-11-03 07:34:56 -0400

    Update general.sh

--- setup_scripts/general.sh
+++ setup_scripts/general.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -x
 
+#
+# setup Anaconda
+#
+wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh
+sudo bash -c "bash Anaconda3-5.3.0-Linux-x86_64.sh -b -p /opt/anaconda3"
+sudo bash -c "echo 'ANACONDA_HOME=/opt/anaconda3/' >> /etc/profile"
+sudo bash -c "echo 'PATH=/opt/anaconda3/bin:$PATH' >> /etc/profile"
+
 # create a user named seed with password dees. 
 sudo useradd -m -p WchOyJRR.1Qrc -s /bin/bash seed
 
