6bc2dbcb8112bd499b3a16e984258605f5bf8348
Linh B Ngo <lngo@clemson.edu>
2018-11-03 07:35:03 -0400

    Update target.sh

--- setup_scripts/packet_sniffing/target.sh
+++ setup_scripts/packet_sniffing/target.sh
@@ -2,14 +2,6 @@
 set -x
 
 #
-# setup Anaconda
-#
-wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh
-sudo bash -c "bash Anaconda3-5.3.0-Linux-x86_64.sh -b -p /opt/anaconda3"
-sudo bash -c "echo 'ANACONDA_HOME=/opt/anaconda3/' >> /etc/profile"
-sudo bash -c "echo 'PATH=/opt/anaconda3/bin:$PATH' >> /etc/profile"
-
-#
 # setup scapy
 #
 sudo /opt/anaconda3/bin/pip install scapy
