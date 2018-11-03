fe28811907852aac1eb495ba864a1067f14f71b4
Linh B Ngo <lngo@clemson.edu>
2018-11-03 02:06:43 -0400

    Update target.sh

--- setup_scripts/packet_sniffing/target.sh
+++ setup_scripts/packet_sniffing/target.sh
@@ -5,15 +5,16 @@ set -x
 # setup Anaconda
 #
 wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh
-bash Anaconda3-5.3.0-Linux-x86_64.sh -b
-echo "export PATH=/home/seed/anaconda3/bin:$PATH" >> .bashrc
-source ~/.bashrc
+sudo bash -c "bash Anaconda3-5.3.0-Linux-x86_64.sh -b -p /opt/anaconda3"
+sudo bash -c "echo 'ANACONDA_HOME=/opt/anaconda3/' >> /etc/profile"
+sudo bash -c "echo 'PATH=/opt/anaconda3/bin:$PATH' >> /etc/profile"
 
 #
 # setup scapy
 #
+sudo /opt/anaconda3/bin/pip install scapy
 
-# work around due to https://github.com/conda/conda/issues/7267
-echo "unset SUDO_UID SUDO_GID SUDO_USER" >> .bashrc
-
-echo "Run infinite UPD program in background"
+#
+# send infite UDP messages to instructor's machine
+#
+sudo /opt/anaconda3/bin/python /local/repository/setup_scripts/packet_sniffing/udp_sender.py > /dev/null 2>&1 &
