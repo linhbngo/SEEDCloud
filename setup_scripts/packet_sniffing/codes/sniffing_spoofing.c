ac6ec5694e111aade123098cadb751d01a94621e
Linh B Ngo <lngo@clemson.edu>
2018-11-05 20:54:16 -0500

    Update sniffing_spoofing.c

--- setup_scripts/packet_sniffing/codes/sniffing_spoofing.c
+++ setup_scripts/packet_sniffing/codes/sniffing_spoofing.c
@@ -66,7 +66,7 @@ void spoof_reply(struct ipheader* ip)
     int ip_header_len = ip->iph_ihl * 4;
     struct udpheader* udp = (struct udpheader *) ((u_char *)ip + 
                                                   ip_header_len);
-    if (ntohs(udp->udp_dport) != 9090) {
+    if (ntohs(udp->udp_dport) != 9999) {
         // Only spoof UDP packet with destination port 9090
         return;
     }
