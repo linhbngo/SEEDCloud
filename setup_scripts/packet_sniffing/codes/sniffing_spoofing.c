c425912e6794ac127189826d42d9e7390d65816f
Linh B Ngo <lngo@clemson.edu>
2018-11-05 22:28:32 -0500

    Update sniffing_spoofing.c

--- setup_scripts/packet_sniffing/codes/sniffing_spoofing.c
+++ setup_scripts/packet_sniffing/codes/sniffing_spoofing.c
@@ -66,9 +66,12 @@ void spoof_reply(struct ipheader* ip)
     int ip_header_len = ip->iph_ihl * 4;
     struct udpheader* udp = (struct udpheader *) ((u_char *)ip + 
                                                   ip_header_len);
-    if (ntohs(udp->udp_dport) != 9999) {
+    if (ntohs(udp->udp_dport) != 9090) {
         // Only spoof UDP packet with destination port 9090
         return;
+    } else {
+      printf("Got one! \n");
+      printf("This is from %s to %s\n", inet_ntoa(ip->iph_sourceip), inet_ntoa(ip->iph_destip));
     }
 
     // Step 1: Make a copy from the original packet 
