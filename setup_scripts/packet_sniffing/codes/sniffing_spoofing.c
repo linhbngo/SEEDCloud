77f2c6826c482d5f9c66718f517aad06b2b9835d
Linh B Ngo <lngo@clemson.edu>
2018-11-05 20:37:55 -0500

    Update sniffing_spoofing.c

--- setup_scripts/packet_sniffing/codes/sniffing_spoofing.c
+++ setup_scripts/packet_sniffing/codes/sniffing_spoofing.c
@@ -25,37 +25,6 @@ struct ipheader {
   struct  in_addr    iph_destip;   //Destination IP address 
 };
 
-
-void got_packet(u_char *args, const struct pcap_pkthdr *header, 
-                              const u_char *packet)
-{
-  struct ethheader *eth = (struct ethheader *)packet;
-
-  if (ntohs(eth->ether_type) == 0x0800) { // 0x0800 is IP type
-    struct ipheader * ip = (struct ipheader *)
-                           (packet + sizeof(struct ethheader)); 
-
-    printf("       From: %s\n", inet_ntoa(ip->iph_sourceip));  
-    printf("         To: %s\n", inet_ntoa(ip->iph_destip));   
-
-    /* determine protocol */
-    switch(ip->iph_protocol) {                               
-        case IPPROTO_TCP:
-            printf("   Protocol: TCP\n");
-            return;
-        case IPPROTO_UDP:
-            printf("   Protocol: UDP\n");
-            return;
-        case IPPROTO_ICMP:
-            printf("   Protocol: ICMP\n");
-            return;
-        default:
-            printf("   Protocol: others\n");
-            return;
-    }
-  }
-}
-
 void send_raw_ip_packet(struct ipheader* ip)
 {
     struct sockaddr_in dest_info;
@@ -118,15 +87,45 @@ void spoof_reply(struct ipheader* ip)
     send_raw_ip_packet(newip);
 }
 
+void got_packet(u_char *args, const struct pcap_pkthdr *header, 
+                              const u_char *packet)
+{
+  struct ethheader *eth = (struct ethheader *)packet;
+
+  if (ntohs(eth->ether_type) == 0x0800) { // 0x0800 is IP type
+    struct ipheader * ip = (struct ipheader *)
+                           (packet + sizeof(struct ethheader)); 
+
+    printf("       From: %s\n", inet_ntoa(ip->iph_sourceip));  
+    printf("         To: %s\n", inet_ntoa(ip->iph_destip));   
+
+    /* determine protocol */
+    switch(ip->iph_protocol) {                               
+        case IPPROTO_TCP:
+            printf("   Protocol: TCP\n");
+            return;
+        case IPPROTO_UDP:
+            printf("   Protocol: UDP\n");
+            spoof_reply(ip);
+            return;
+        case IPPROTO_ICMP:
+            printf("   Protocol: ICMP\n");
+            return;
+        default:
+            printf("   Protocol: others\n");
+            return;
+    }
+  }
+}
+
+
 int main()
 {
   pcap_t *handle;
   char errbuf[PCAP_ERRBUF_SIZE];
   struct bpf_program fp;
-  char filter_exp[] = "ip proto icmp";
-  bpf_u_int32 net;
 
-  handle = pcap_open_live("eth3", BUFSIZ, 1, 1000, errbuf);               
+  handle = pcap_open_live("eth1", BUFSIZ, 1, 1000, errbuf);               
 
   pcap_loop(handle, -1, got_packet, NULL);                
 
