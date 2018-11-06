a2abe34134ea4d09f5394f0f1a6619da5fd20cd9
Linh B Ngo <lngo@clemson.edu>
2018-11-05 23:10:43 -0500

    Update target.sh

--- setup_scripts/packet_sniffing/target.sh
+++ setup_scripts/packet_sniffing/target.sh
@@ -9,4 +9,4 @@ sudo /opt/anaconda3/bin/pip install scapy
 #
 # send infite UDP messages to instructor's machine
 #
-sudo /opt/anaconda3/bin/python /local/repository/setup_scripts/packet_sniffing/udp_sender.py > /dev/null 2>&1 &
+# sudo /opt/anaconda3/bin/python /local/repository/setup_scripts/packet_sniffing/udp_sender.py > /dev/null 2>&1 &
