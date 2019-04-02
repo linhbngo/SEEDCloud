4efbe2176497f38a1ee3356f3eb166bd48d86f41
Linh B Ngo <lngo@clemson.edu>
2019-04-02 13:17:39 -0400

    Update general.sh

--- setup_scripts/general.sh
+++ setup_scripts/general.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 set -x
 
-# update Ubuntu's repository
-sudo apt-get -y update
-
 # create a user named seed with password dees. 
 sudo useradd -m -p WchOyJRR.1Qrc -s /bin/bash seed
-
-# add seed to sudo
 sudo usermod -a -G sudo seed
 
 # activate password connection
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sudo service ssh restart
+
+# update Ubuntu's repository
+sudo apt-get -y update
 
 # setup Docker
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common tmux
