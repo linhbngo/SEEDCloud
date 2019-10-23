e568575604e257b76def90d22a139206c73507df
Linh B Ngo <lngo@wcupa.edu>
2019-10-23 13:33:46 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -11,69 +11,32 @@ import random
 import os.path
 import sys
 
-TBURL = "http://www.emulab.net/downloads/openstack-setup-v33.tar.gz"
-TBPERM = "sudo chmod -R 755 /local/repository/*.sh"
 IMAGE = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
 IMAGE_ARM = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-ARM"
 
-#
-# For now, disable the testbed's root ssh key service until we can remove ours.
-# It seems to race (rarely) with our startup scripts.
-#
 disableTestbedRootKeys = True
-
-#
-# Create our in-memory model of the RSpec -- the resources we're going to request
-# in our experiment, and their configuration.
-#
 rspec = RSpec.Request()
-
-#
-# This geni-lib script is designed to run in the CloudLab Portal.
-#
 pc = portal.Context()
 
-#
-# Define *many* parameters; see the help docs in geni-lib to learn how to modify.
-#
-
 pc.defineParameter("seedlabtype","SEED Lab",
-                   portal.ParameterType.STRING,"software",[("software","Software Security"),("web","Web Security"),("network","Network Security")])
-pc.defineParameter("studentCount", "Number of students",
+                   portal.ParameterType.STRING,"software",
+                   [("software","Software Security"),
+                    ("web","Web Security"),
+                    ("network","Network Security")])
+pc.defineParameter("machines", "Number of physical nodes",
                    portal.ParameterType.INTEGER, 1)
-pc.defineParameter("raw", "Use physical nodes",
-                    portal.ParameterType.BOOLEAN, False)
-pc.defineParameter("arm", "Use Utah's ARM",
-                    portal.ParameterType.BOOLEAN, False)
-
-#
-# Get any input parameter values that will override our defaults.
-#
 params = pc.bindParameters()
-
-#
-# Give the library a chance to return nice JSON-formatted exception(s) and/or
-# warnings; this might sys.exit().
-#
 pc.verifyParameters()
 
 tourDescription = \
-  "This profile provides a highly-configurable SEED Lab infrastructure"
-#
-# Setup the Tour info with the above description and instructions.
-#  
+  "This profile provides a configurable SEED Lab infrastructure"
 tour = IG.Tour()
 tour.Description(IG.Tour.TEXT,tourDescription)
 #tour.Instructions(IG.Tour.MARKDOWN,tourInstructions)
 rspec.addTour(tour)
 
 def Node(name):
-  if params.raw:
-    newnode = RSpec.RawPC(name)
-  else:
-    newnode = IG.XenVM(name)
-    newnode.ram = 2048
-    newnode.cores = 2
+  newnode = RSpec.RawPC(name)
   newnode.routable_control_ip = True
   return newnode                    
 
@@ -83,23 +46,20 @@ rspec.addResource(lan)
 prefixForIP = "192.168.1."
 local_ip_count = 0                   
 for i in range(params.studentCount + 1):
-  if params.seedlabtype == "software":
-    if i == 0:
-      node = Node("instructor")
-    else:
-      node = Node("lab_instance_"+str(i))
-    
-    if params.arm:
-      node.disk_image = IMAGE_ARM
-    else:
-      node.disk_image = IMAGE
-    local_ip_count += 1                    
-    iface = node.addInterface("if" + str(local_ip_count))
-    iface.component_id = "eth1"
-    iface.addAddress(RSpec.IPv4Address(prefixForIP + str(local_ip_count), "255.255.255.0"))
-    lan.addInterface(iface)
-    node.addService(RSpec.Execute("sh", "sudo bash /local/repository/setup_scripts/general.sh"))
-    node.addService(RSpec.Execute("sh", "sudo bash /local/repository/setup_scripts/software.sh"))
-    rspec.addResource(node)   
+  if i == 0:
+    node = Node("head")
+  else:
+    node = Node("worker_"+str(i))
+  node.disk_image = IMAGE
+  local_ip_count += 1                    
+  iface = node.addInterface("if" + str(local_ip_count))
+  iface.component_id = "eth1"
+  iface.addAddress(RSpec.IPv4Address(prefixForIP + str(local_ip_count), "255.255.255.0"))
+  lan.addInterface(iface)
+  node.addService(RSpec.Execute("sh", "sudo bash /local/repository/setup_scripts/install_docker.sh"))
+  #node.addService(RSpec.Execute("sh", "sudo bash /local/repository/setup_scripts/general.sh"))
+  #node.addService(RSpec.Execute("sh", "sudo bash /local/repository/setup_scripts/software.sh"))
+  rspec.addResource(node)   
+  #if params.seedlabtype == "software":
          
 pc.printRequestRSpec(rspec)
