347cf5810fadc267f335bc1d67ae23bcc1ec20d7
Linh B Ngo <lngo@wcupa.edu>
2019-10-24 15:09:30 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -49,7 +49,7 @@ for i in range(params.machines):
   if i == 0:
     node = Node("head")
   else:
-    node = Node("worker_"+str(i))
+    node = Node("worker_"+str(i + 1))
   node.disk_image = IMAGE
   local_ip_count += 1                    
   iface = node.addInterface("if" + str(local_ip_count))
@@ -57,8 +57,18 @@ for i in range(params.machines):
   iface.addAddress(RSpec.IPv4Address(prefixForIP + str(local_ip_count), "255.255.255.0"))
   lan.addInterface(iface)
   node.addService(RSpec.Execute("sh", "sudo bash /local/repository/setup_scripts/install_docker.sh"))
-  #node.addService(RSpec.Execute("sh", "sudo bash /local/repository/setup_scripts/general.sh"))
-  #node.addService(RSpec.Execute("sh", "sudo bash /local/repository/setup_scripts/software.sh"))
+  
+  if i == 0:
+    node.addService(RSpec.Execute("sh", "sudo apt-get install -y nfs-kernel-server"))
+    node.addService(RSpec.Execute("sh", "sudo mkdir -p /opt/keys"))
+    node.addService(RSpec.Execute("sh", "sudo chown nobody:nogroup /opt/keys"))
+    for i in range(1,params.machines):
+      script_line = 'sudo echo "/opt/keys 192.168.1.' + str(i + 1) + '(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports'
+      node.addService(RSpec.Execute("sh", script_line))
+  else:
+    node.addService(RSpec.Execute("sh", "sudo apt-get install -y nfs-common"))
+    node.addService(RSpec.Execute("sh", "sudo mkdir -p /opt/keys"))
+    node.addService(RSpec.Execute("sh", "sudo mount 192.168.1.1:/opt/keys /opt/keys"))
   rspec.addResource(node)   
   #if params.seedlabtype == "software":
          
