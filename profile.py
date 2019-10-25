c429c95a09f446c8479d86df07460531a93f1ef0
Linh B Ngo <lngo@wcupa.edu>
2019-10-25 12:02:00 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -63,8 +63,8 @@ for i in range(params.machines):
     node.addService(RSpec.Execute("sh", "sudo apt-get install -y nfs-kernel-server"))
     node.addService(RSpec.Execute("sh", "sudo mkdir -p /opt/keys"))
     node.addService(RSpec.Execute("sh", "sudo chown nobody:nogroup /opt/keys"))
-    for i in range(1,params.machines):
-      node.addService(RSpec.Execute("sh", "sudo echo '/opt/keys 192.168.1." + str(i+1) + "(rw,sync,no_root_squash,no_subtree_check)' | sudo tee -a /etc/exports"))
+    for k in range(1,params.machines):
+      node.addService(RSpec.Execute("sh", "sudo echo '/opt/keys 192.168.1." + str(k+1) + "(rw,sync,no_root_squash,no_subtree_check)' | sudo tee -a /etc/exports"))
     node.addService(RSpec.Execute("sh", "sudo systemctl restart nfs-kernel-server"))
   else:
     node.addService(RSpec.Execute("sh", "sudo apt-get update"))
