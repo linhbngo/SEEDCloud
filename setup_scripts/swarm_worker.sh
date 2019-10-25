f95636abcc299d1a2c8b0cbe2524ba65a349aaa8
Linh B Ngo <lngo@wcupa.edu>
2019-10-25 17:38:28 -0400

    Update swarm_worker.sh

--- setup_scripts/swarm_worker.sh
+++ setup_scripts/swarm_worker.sh
@@ -2,4 +2,6 @@
 set -x
 
 sleep 5m
-sudo docker swarm join --token `more /opt/keys/docker.swarm` 192.168.1.1:7777
+dtoken=`cat /opt/keys/docker.swam`
+echo $dtoken
+sudo docker swarm join --token $dtoken 192.168.1.1:7777
