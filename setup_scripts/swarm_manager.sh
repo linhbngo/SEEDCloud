e03c123d3027cf01e65dfedf978f87f018e5a3c8
Linh B Ngo <lngo@wcupa.edu>
2019-10-25 13:33:26 -0400

    Create swarm_manager.sh

---
+++ setup_scripts/swarm_manager.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+  
+sudo -H -u lngo bash -c "docker swarm init --advertise-addr 192.168.1.1:7777 --listen-addr 192.168.1.1:7777 > ~/swarm_server"
+sudo -H -u lngo bash -c "more ~/swarm_server | grep 7777 | cut -d' ' -f9 | sudo tee -a /opt/keys/docker.swarm"))
