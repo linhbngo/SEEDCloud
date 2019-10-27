ea28399f8d6845170e1cc693a1056ec1a860f8d1
Linh B Ngo <lngo@wcupa.edu>
2019-10-27 16:24:37 -0400

    Update swarm_manager.sh

--- setup_scripts/swarm_manager.sh
+++ setup_scripts/swarm_manager.sh
@@ -3,3 +3,6 @@ set -x
   
 sudo -H -u lngo bash -c "docker swarm init --advertise-addr 192.168.1.1:7777 --listen-addr 192.168.1.1:7777 > ~/swarm_server"
 sudo -H -u lngo bash -c "more ~/swarm_server | grep 7777 | cut -d' ' -f9 | sudo tee -a /opt/keys/docker.swarm"
+sudo -H -u lngo bash -c "docker network create --driver overlay seednet"
+"
+
