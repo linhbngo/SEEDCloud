b59368792adf12bdfcdf2c7ef0b7bb5a73f55197
Linh B Ngo <lngo@wcupa.edu>
2019-10-26 20:14:01 -0400

    Update launch_dirtycow.sh

--- setup_scripts/launch_dirtycow.sh
+++ setup_scripts/launch_dirtycow.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
 echo $1
-docker service create -d -p ' + $1:8888 linhbngo/csc302:software.1204.dirtycow.0.9
+docker service create -d -p $1:8888 linhbngo/csc302:software.1204.dirtycow.0.9
