5b7cde8ffaf1451994486948162bf6e85ec2fde3
Linh B Ngo <lngo@wcupa.edu>
2023-01-30 15:12:17 -0500

    Update general.sh

--- setup_scripts/general.sh
+++ setup_scripts/general.sh
@@ -13,13 +13,35 @@ sudo service ssh restart
 sudo apt-get -y update
 
 # setup Docker
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common tmux
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
-sudo apt-get install -y httping
-sudo apt-get install -y jq
+sudo apt-get install -y binutils \
+        curl   \
+        iproute2  \
+        iputils-ping \
+        nano   \
+        net-tools \
+        unzip \ 
+        arping \
+        conntrack \
+        curl   \
+        dnsutils  \
+        iptables \
+        mtr-tiny  \
+        netcat \
+        openbsd-inetd  \
+        procps \
+        tcpdump   \
+        telnet \
+        telnetd \
+        python3.8-distutils \
+     && rm -rf /var/lib/apt/lists/*
+     
+     cd /local/repository/
+     python3 /local/repository/get-pip3.py \
+     pip3 install scapy
+     
 
-# the username needs to be changed
-sudo usermod -aG docker seed
+sudo cp bashrc /home/seed/.bashrc
+chown seed: /home/seed/.bashrc
+sudo cp bashrc /root/.bashrc
+
+CMD /bin/bash
