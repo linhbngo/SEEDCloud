9c23243435b43bc4a9ac0247ff11776cfb98448d
Linh B Ngo <lngo@wcupa.edu>
2023-01-30 15:38:21 -0500

    Update general.sh

--- setup_scripts/general.sh
+++ setup_scripts/general.sh
@@ -2,44 +2,28 @@
 set -x
 
 # create a user named seed with password dees. 
-sudo useradd -m -p WchOyJRR.1Qrc -s /bin/bash seed
-sudo usermod -a -G sudo seed
+useradd -m -p WchOyJRR.1Qrc -s /bin/bash seed
+usermod -a -G sudo seed
 
 # activate password connection
-sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-sudo service ssh restart
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+service ssh restart
 
 # update Ubuntu's repository
-sudo apt-get -y update
+DEBIAN_FRONTEND=noninteractive
+apt-get -y update
 
 # setup Docker
-sudo apt-get install -y binutils \
-        curl   \
-        iproute2  \
-        iputils-ping \
-        nano   \
-        net-tools \
-        unzip \ 
-        arping \
-        conntrack \
-        curl   \
-        dnsutils  \
-        iptables \
-        mtr-tiny  \
-        netcat \
-        openbsd-inetd  \
-        procps \
-        tcpdump   \
-        telnet \
-        telnetd \
-        python3.8-distutils \
-     && rm -rf /var/lib/apt/lists/*
+apt-get install -y binutils curl iproute2 iputils-ping nano
+apt-get install -y net-tools unzip arping conntrack curl dnsutils 
+apt-get install -y iptables mtr-tiny netcat openbsd-inetd procps
+apt-get install -y tcpdump telnet telnetd python3.8-distutils
+rm -rf /var/lib/apt/lists/*
      
-     cd /local/repository/
-     python3 /local/repository/get-pip3.py \
-     pip3 install scapy
+python3 /local/repository/get-pip3.py 
+pip3 install scapy
      
 
-sudo cp bashrc /home/seed/.bashrc
+cp bashrc /home/seed/.bashrc
 chown seed: /home/seed/.bashrc
-sudo cp bashrc /root/.bashrc
+cp bashrc /root/.bashrc
