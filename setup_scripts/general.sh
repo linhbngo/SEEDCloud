76e850ee80ce9ea6a0d791723e02d6469bc672c7
Linh B Ngo <lngo@wcupa.edu>
2023-01-31 21:31:00 -0500

    Update general.sh

--- setup_scripts/general.sh
+++ setup_scripts/general.sh
@@ -13,13 +13,5 @@ sudo service ssh restart
 sudo apt-get -y update
 
 # setup Docker
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common tmux
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
-sudo apt-get install -y httping
-sudo apt-get install -y jq
+sudo apt-get install -y tmux
 
-# the username needs to be changed
-sudo usermod -aG docker seed
