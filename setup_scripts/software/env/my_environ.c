8ca5e5fc051dee90cd8e8b866f394e7ace7ef0a3
Linh B Ngo <lngo@wcupa.edu>
2023-02-05 10:22:53 -0500

    Create my_environ.c

---
+++ setup_scripts/software/env/my_environ.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+extern char** environ;
+
+void main(int argc, char* argv[], char* envp[]) {
+  int i = 0;
+  while (environ[i] != NULL) {
+    printf("%s\n", environ[i++];
+  }
+}
